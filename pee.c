fa7787ccb87cc8c47f118c65d5bbada602323a68
joeyh <joeyh>
2006-03-16 17:08:29 +0000

    er yeah, let's check in the right stuff

---
+++ pee.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Licensed under the GPL
+ * Copyright (c) Miek Gieben, 2006
+ */
+
+/* like tee(1), but then connect to other programs using
+ * pipes _and_ output to standard output
+ */
+
+void
+close_pipes(FILE **p, size_t i) 
+{
+	size_t j;
+	for (j = 0; j < i; j++) 
+		pclose(p[j]);
+}
+
+int
+main(int argc, char **argv) {
+	size_t i, r;
+	FILE **pipes;
+	char buf[BUFSIZ];
+
+	pipes = malloc(((argc - 1) * sizeof *pipes));
+	if (!pipes) 
+		exit(EXIT_FAILURE);
+
+	for (i = 1; i < argc; i++) {
+		pipes[i - 1] = popen(argv[i], "w");
+		if (!pipes[i - 1]) {
+			fprintf(stderr, "Can not open pipe to '%s\'\n", argv[i]);
+			close_pipes(pipes, i);
+
+			exit(EXIT_FAILURE);
+		}
+	}
+	argc--;
+	
+	while(!feof(stdin) && (!ferror(stdin))) {
+		r = fread(buf, sizeof(char), BUFSIZ, stdin);
+		for(i = 0; i < argc; i++) {
+			if (fwrite(buf, sizeof(char), r, pipes[i]) != r) {
+				fprintf(stderr, "Write error to `%s\'\n", argv[i + 1]);
+				close_pipes(pipes, i);
+				exit(EXIT_FAILURE);
+			}
+			fwrite(buf, sizeof(char), r, stdout);
+		}
+	}
+	close_pipes(pipes, argc);
+
+	exit(EXIT_SUCCESS);
+}

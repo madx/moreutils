madx/moreutils

commit 6f31909ff74c064ea0b5126219b3e8f7b8826bee

optimize tempfile copying
Reuse the buffer and copy in chunks that are the full buffer size.

sponge.c
@@ -199,15 +199,14 @@ static void write_buff_out(char* buff, size_t length, FILE *fd) {
 	}
 }
 
-static void copy_tmpfile(FILE *tmpfile, FILE *outfile) {
-	char buf[BUFF_SIZE];
+static void copy_tmpfile(FILE *tmpfile, FILE *outfile, char *buf, size_t size) {
 	if (fseek(tmpfile, 0, SEEK_SET)) {
 		perror("could to seek to start of temporary file");
 		fclose(tmpfile);
 		exit(1);
 	}
-	while (fread(buf, BUFF_SIZE, 1, tmpfile) > 0) {
-		write_buff_out(buf, BUFF_SIZE, outfile);
+	while (fread(buf, size, 1, tmpfile) > 0) {
+		write_buff_out(buf, size, outfile);
 	}
 	if (ferror(tmpfile)) {
 		perror("read temporary file");
@@ -293,7 +292,6 @@ int main (int argc, char **argv) {
 		/* write whatever we have in memory to tmpfile */
 		if (bufused) 
 			write_buff_tmp(bufstart, bufused, tmpfile);
-		free(bufstart);
 		if (fflush(tmpfile) != 0) {
 			perror("fflush");
 			exit(1);
@@ -322,10 +320,10 @@ int main (int argc, char **argv) {
 				perror("error opening output file");
 				exit(1);
 			}
-			copy_tmpfile(tmpfile, outfile);
+			copy_tmpfile(tmpfile, outfile, bufstart, bufsize);
 		}
 		else {
-			copy_tmpfile(tmpfile, stdout);
+			copy_tmpfile(tmpfile, stdout, bufstart, bufsize);
 		}
 	}
 	else {

commit a456619d189cef2933aefef86606ae4b29516c85

sponge: Correct bad use of fread that caused a trailing quantity of soaked data to be silently discarded when a temp file was used and sponge output to stdout. Closes: #595220
This bug was sorta introduced by 6f31909ff74c064ea0b5126219b3e8f7b8826bee.

Actually, the buggy fread was there before also, and would have happened
on quantities of data not evenly divisible by 8.

sponge.c
@@ -199,15 +199,16 @@ static void write_buff_out(char* buff, size_t length, FILE *fd) {
 }
 
 static void copy_tmpfile(FILE *tmpfile, FILE *outfile, char *buf, size_t size) {
-	if (fseek(tmpfile, 0, SEEK_SET)) {
+	ssize_t i;
+	if (lseek(fileno(tmpfile), 0, SEEK_SET)) {
 		perror("could to seek to start of temporary file");
 		fclose(tmpfile);
 		exit(1);
 	}
-	while (fread(buf, size, 1, tmpfile) > 0) {
-		write_buff_out(buf, size, outfile);
+	while ((i = read(fileno(tmpfile), buf, size)) > 0) {
+		write_buff_out(buf, i, outfile);
 	}
-	if (ferror(tmpfile)) {
+	if (i == -1) {
 		perror("read temporary file");
 		fclose(tmpfile);
 		exit(1);